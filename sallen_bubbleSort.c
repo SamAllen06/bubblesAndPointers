1fb32c3014b227594c58c72203d9040780c695ec
SamAllen06 <samgallen06@gmail.com>
2025-01-22 14:31:16 -0500

    changed bubble sort array and output to resemble the one on canvas

--- sallen_bubbleSort.c
+++ sallen_bubbleSort.c
@@ -1,17 +1,34 @@
 //Bubble Sort
 #include <stdio.h>
 
-#define MAX 10
+#define MAX 9
 void printArray(int*);
 void sort(int*);
 void swap(int*, int*);
 
+int main(){
+	int array[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
+	printf("Before: \n");
+	printArray(array);
+	
+	int x = 3;
+	int y = 5;
+	printf("x: %d, y: %d \n", x, y);
+	swap(&x, &y);
+	printf("x: %d, y: %d \n", x, y);
+
+	sort(array);
+	printf("After: \n");
+	printArray(array);
+
+	return 0;
+}//end main
 
 void sort(int *array){
 	int i;
 	int j;
 	for (i=0; i<MAX; i++){
-		for (j=0; j<MAX; j++){
+		for (j=0; j<(MAX-1); j++){
 			if (array[j]>array[j+1]){
 				swap(&array[j], &array[j+1]);
 				printArray(array);
@@ -39,19 +56,4 @@ void swap(int *a, int *c){
 	*c = b;
 } //end swap function
 
-int main(){
-	int array[] = {10, 5, 3, 7, 2, 9, 1, 4, 6, 8};
-	printf("Before: ");
-	printArray(array);
-	int x = 5;
-	int y = 6;
-	printf("x: %d, y: %d \n", x, y);
-	swap(&x, &y);
-	printf("x: %d, y: %d \n", x, y);
 
-	sort(array);
-	printf("After: ");
-	printArray(array);
-
-	return 0;
-}//end main
